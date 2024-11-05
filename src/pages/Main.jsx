458430a0052b6af7fe04c8f0b5c63a2ea090298e
Mohammad-200 <mohammad.kizawi12@e-uvt.ro>
2024-11-06 00:46:28 +0200

    adding universities and companies i worked for

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,47 +3,57 @@ import "./main.css";
 
 function Main() {
   return (
-    <div className="main-container">
-      <div className="centered-container">
-        <div className="main-left">
-          <div>
-            <label>Profile</label>
-            <p>
-              Work for money and program for love! I'm Mohammad, a
-              <span> Full stack</span> developer based in Romania
-            </p>
+    <>
+      <div className="main-container">
+        <div className="centered-container">
+          <div className="main-left">
+            <div>
+              <label>Profile</label>
+              <p>
+                Work for money and code for love! I'm Mohammad, a
+                <span> Full stack</span> developer based in Romania
+              </p>
+            </div>
+            <div>
+              <label>Contact</label>
+              <p>mohammad.kizawi@gmail.com</p>
+              <p>+40724952701</p>
+            </div>
+            <div>
+              <label>Services</label>
+              <p>Frontend developer</p>
+              <p>backend developer</p>
+            </div>
           </div>
-          <div>
-            <label>Contact</label>
-            <p>mohammad.kizawi@gmail.com</p>
-            <p>+40724952701</p>
+          <div className="main-pic">
+            <img src="images/personal-image.jpeg" />
+            <div className="image-border"></div>
           </div>
-          <div>
-            <label>Services</label>
-            <p>Frontend developer</p>
-            <p>backend developer</p>
+          <div className="main-right" style={{ textAlign: "right" }}>
+            <div>
+              <label>Years of experience</label>
+              <p className="big-font">2</p>
+            </div>
+            <div>
+              <label>Work Satisfaction</label>
+              <p className="big-font">100%</p>
+            </div>
+            <div>
+              <label>Projects done</label>
+              <p className="big-font">+100</p>
+            </div>
           </div>
         </div>
-        <div className="main-pic">
-          <img src="images/personal-image.jpeg" />
-          <div className="image-border"></div>
-        </div>
-        <div className="main-right" style={{ textAlign: "right" }}>
-          <div>
-            <label>Years of experience</label>
-            <p className="big-font">2</p>
-          </div>
-          <div>
-            <label>Work Satisfaction</label>
-            <p className="big-font">100%</p>
-          </div>
-          <div>
-            <label>Projects done</label>
-            <p className="big-font">+100</p>
-          </div>
+        <div className="main-companies-logos">
+          <img src="images/uvt-logo.png" alt="" />
+          <img src="images/jku-logo.png" alt="" />
+          <img src="images/kiska-logo.png" alt="" style={{ width: "150px" }} />
+          <img src="images/ktm-logo.png" alt="" />
+          <img src="images/gasgas-logo.png" alt="" />
         </div>
       </div>
-    </div>
+      <div className="end-line"></div>
+    </>
   );
 }
 
