e9d18662b9e5d8dc0164a3fe6427767ecb3d1e33
Mohammad-200 <mohammad.kizawi12@e-uvt.ro>
2024-11-06 01:44:07 +0200

    adding experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import Experience from "./pages/Experience";
 import Main from "./pages/Main";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Experience />
     </>
   );
 }
